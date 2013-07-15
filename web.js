581dd3e1f38ce5ad1d361f63258d9042dc2a6e56
Florent Muyango <muyango3@gmail.com>
2013-07-15 03:41:57 +0000

    updated it again

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var buf= new Buffer(256);
+buf = fs.readFileSync('index.html'); //no encoding to return a buffer
+
+var string= buf.toString('utf-8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+	response.send(string);
 });
 
 var port = process.env.PORT || 5000;
