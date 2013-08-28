3e3499667949bdbf1fcc5001d07c6f13c97b2b64
fmcyamwe <muyango3@gmail.com>
2013-08-28 03:33:34 +0000

    Added a service tab.

--- web.js
+++ web.js
@@ -1,16 +1,14 @@
-var express = require("express");
-
+var express = require('express');
 var fs = require('fs');
+var htmlfile="index.html";
 
-var app = express(express.logger());
-var buf= new Buffer(256);
-buf = fs.readFileSync('index.html'); //no encoding to return a buffer
-
-var string= buf.toString('utf-8');
+var app = express.createServer(express.logger());
+//var buf= new Buffer(256);
 
 app.get('/', function(request, response) {
-  //response.send('Hello World 2!');
-	response.send(string);
+ 	var html=fs.readFileSync(htmlfile).toString();
+
+	response.send(html);
 });
 
 var port = process.env.PORT || 8080;
